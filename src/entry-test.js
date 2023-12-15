024575ba05232b3ddc797f91717ed654b9c81bdc
Bill Harding <wbharding@gmail.com>
2023-12-15 15:49:43 -0800

    WIP allow constants to be inlined along with functions

--- src/entry-test.js
+++ src/entry-test.js
@@ -1,3 +1,4 @@
+import { CODE_HEADING, DEFAULT_BRANCH, MAX_REPLACE_CONTENT_LENGTH, ENTRY_LOCATIONS } from "lib/plugin-constants"
 import { inlineImportsFromGithub } from "lib/plugin-import-inliner"
 import { wrappedFetch } from "./nested-import"
 
@@ -6,6 +7,10 @@ const plugin = {
     const entryPoint = {};
     const codeObject = "";
     const inlineImports = await inlineImportsFromGithub(entryPoint, codeObject, {});
+    const heading = CODE_HEADING;
+    const branch = DEFAULT_BRANCH;
+    const replaceLength = MAX_REPLACE_CONTENT_LENGTH;
+    const locations = ENTRY_LOCATIONS;
     wrappedFetch("https://gitclear.com", {});
   }
 }
