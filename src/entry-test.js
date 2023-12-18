e3a3d6f1d7933fd4566f1c9c2aeaad31a8d794bd
Bill Harding <wbharding@gmail.com>
2023-12-17 17:57:51 -0800

    Separate constant test into its own entry file

--- src/entry-test.js
+++ src/entry-test.js
@@ -1,4 +1,3 @@
-import { CODE_HEADING, DEFAULT_BRANCH, MAX_REPLACE_CONTENT_LENGTH, ENTRY_LOCATIONS } from "lib/plugin-constants"
 import { inlineImportsFromGithub } from "lib/plugin-import-inliner"
 import { wrappedFetch } from "./nested-import"
 
@@ -8,10 +7,6 @@ const plugin = {
     const codeObject = "";
     // I will stay on line, promises CONSTANT_OBJECT and its buddy CONSTANT_ARRAY
     const inlineImports = await inlineImportsFromGithub(entryPoint, codeObject, [], []);
-    const heading = CODE_HEADING;
-    const branch = DEFAULT_BRANCH;
-    const replaceLength = MAX_REPLACE_CONTENT_LENGTH;
-    const locations = ENTRY_LOCATIONS;
     wrappedFetch("https://gitclear.com", {});
   }
 }
