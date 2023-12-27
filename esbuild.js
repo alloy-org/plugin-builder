eafd4768d6785dc7b65e543d8f8a34ea9591b8ba
Bill Harding <wbharding@gmail.com>
2023-12-27 12:14:06 -0800

    Simplify esbuild.js

--- esbuild.js
+++ esbuild.js
@@ -3,15 +3,13 @@ import esbuild from "esbuild"
 
 dotenv.config();
 
-// Adapted from Lucian
 const result = await esbuild.build({
     entryPoints: [`lib/plugin.js`],
     bundle: true,
     format: "iife",
-    // minify: true,
     outfile: "build/compiled.js",
     packages: "external",
     platform: "node",
     write: true,
 });
-console.log("Result was", result)
+console.log("Build result", result)
